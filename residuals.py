6ce3cfa916cc1d9e03b50b8c01c2f16c2b6576d6
rachelboy <rachel.boy@gmail.com>
2014-09-30 17:39:19 -0400

    residuals

--- residuals.py
+++ residuals.py
@@ -8,11 +8,16 @@ def wrapInterp(val,interpArray):
 	delta = val-floor
 	return interpArray[floor](delta)
 
-def interpolateLazers(lazerData):
+def interpaLazers(lazerData):
 	#assume array goes 1-360
 	dataPairs = zip(lazerData,lazerData[1:]+lazerData[:1])
 	interp = [makeFunc(r1,r2) for r1,r2 in dataPairs]
 	return lambda x: wrapInterp(x,interp)
+
+def computeResidual(old, new, new_thetas):
+	oldInterpFunc = interpaLazers(old)
+	oldInterped = [oldInterpFunc(theta) for theta in new_thetas]	
+	return abs(sum([o-n for o,n in zip(oldInterped,new)]))
 
 if __name__ == "__main__":
 	pass
--- residuals_test.py
+++ residuals_test.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy
-from residuals import makeFunc, wrapInterp, interpolateLazers
+from residuals import makeFunc, wrapInterp, interpaLazers, computeResidual
 
 
 class TestInterpolation(unittest.TestCase):
@@ -19,10 +19,19 @@ class TestInterpolation(unittest.TestCase):
     	self.assertEqual(wrapInterp(0.3, interpArray), 1.3)
     	self.assertEqual(wrapInterp(1.3, interpArray), 4.3)
 
-    def test_interpolateLazers(self):
-        interp = interpolateLazers(range(0, 360))
+    def test_interpaLazers(self):
+        interp = interpaLazers(range(360))
         self.assertEqual(interp(359.5), 179.5)
         self.assertEqual(interp(182.2365),182.2365)
+
+    def test_computeResidual(self):
+        old = range(360)
+        new = [o+.5 for o in old]
+        new_thetas = list(new)
+        new[-1] = 179.5
+        self.assertEqual(computeResidual(old,new,new_thetas),0)
+        new[0] = 0
+        self.assertEqual(computeResidual(old,new,new_thetas),0.5)
 
 if __name__ == '__main__':
     unittest.main()
