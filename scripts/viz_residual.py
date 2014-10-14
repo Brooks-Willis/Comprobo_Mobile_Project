6bc7057daa4dee319de8f5a8137592c1993bda15
rachelboy <rachel.boy@gmail.com>
2014-10-14 17:24:30 -0400

    plotting (surfaces don't work right now)

---
+++ scripts/viz_residual.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+import tf
+import gradient_descent as grd
+import residuals as rsd
+import matplotlib.pyplot as pp
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose, Point, Quaternion
+
+
+class Lazer(object):
+    def __init__(self):
+        self.sub = rospy.Subscriber('scan', LaserScan, self.scan_received)
+        self.penultimate = None
+        self.ultimate = None
+        self._intermediate = None
+
+    def scan_received(self, data):
+        if data.ranges == self.ultimate:
+            return
+
+        if self.penultimate == None:
+            self.penultimate = self.ultimate
+        self.ultimate = data.ranges
+
+    def get_scans(self):
+        old_scan = self._copy_scan(self.penultimate)
+        new_scan = self._copy_scan(self.ultimate)
+
+        self.penultimate = self.ultimate
+        return old_scan, new_scan
+
+    def _copy_scan(self, scan):
+        if scan == None:
+            return None
+        else:
+            return list(scan)
+
+
+class Odom(object):
+    def __init__(self):
+        self.sub = rospy.Subscriber('odom', Odometry, self.odom_received)
+        self.old_odom = None
+        self.new_odom = None
+          # dx, dy, dtheta
+
+    def deltas(self):
+        if not (self.old_odom and self.new_odom):
+            return None
+
+        out = np.array([[0], [0], [0]])
+
+        out[0,0] += (
+            self.new_odom.pose.pose.position.x - self.old_odom.pose.pose.position.x
+        )
+        out[1,0] += (
+            self.new_odom.pose.pose.position.y - self.old_odom.pose.pose.position.y
+        )
+        new_quat = (self.new_odom.pose.pose.orientation.x,
+            self.new_odom.pose.pose.orientation.y,
+            self.new_odom.pose.pose.orientation.z,
+            self.new_odom.pose.pose.orientation.w)
+        old_quat = (self.old_odom.pose.pose.orientation.x,
+            self.old_odom.pose.pose.orientation.y,
+            self.old_odom.pose.pose.orientation.z,
+            self.old_odom.pose.pose.orientation.w)
+        new_yaw = tf.transformations.euler_from_quaternion(new_quat)[2]
+        old_yaw = tf.transformations.euler_from_quaternion(old_quat)[2]
+        out[2,0] += np.degrees(new_yaw-old_yaw)
+        return out
+
+    def odom_received(self, data):
+        if self.old_odom == None:
+            self.old_odom = data
+        self.new_odom = data
+
+    def get_deltas(self):
+        temp = self.deltas()
+        self.old_odom = self.new_odom
+        return temp
+
+
+class Lazerdom(object):
+    def __init__(self):
+        rospy.init_node('lazerdom', anonymous=True)
+        self.pub = rospy.Publisher('corrected_odom', Pose, queue_size=20)
+        self.lazer_sub = Lazer()
+        self.odom_sub = Odom()
+        self.current_quat = (0,0,0,1)
+        self.current_pos = [0,0,0]
+        self.pose = None
+
+    def compute_new_pose(self, actual_del):
+        dx = actual_del[0,0]
+        dy = actual_del[1,0]
+        dtheta = actual_del[2,0]
+
+        self.current_pos[0] += dx
+        self.current_pos[1] += dy
+
+        quat = tf.transformations.quaternion_from_euler(0,0,np.radians(dtheta))
+        quat /= np.linalg.norm(quat)
+        self.current_quat = tf.transformations.quaternion_multiply(
+            quat,
+            self.current_quat)
+        self.current_quat /= np.linalg.norm(self.current_quat)
+
+        position = Point(x=self.current_pos[0],
+            y=self.current_pos[1],
+            z=self.current_pos[2])
+
+        orientation = Quaternion(x = self.current_quat[0],
+            y = self.current_quat[1],
+            z = self.current_quat[2],
+            w = self.current_quat[3])
+
+        self.pose = Pose(position = position, orientation = orientation)
+
+    def publish_pose(self):
+        self.pub.publish(self.pose)
+
+    def visualize_residuals_2(self,res_func,guess_del):
+        x,y,theta = guess_del
+        xs = [x+.1*i for i in range(-20,20)]
+        ys = [y+.1*i for i in range(-20,20)]
+
+        rs = [[res_func(np.array([[x_i], [y_i], [theta]])) for y_i in ys] for x_i in xs]
+        rs = np.array(rs)
+
+        pp.contourf(xs,ys,rs, label = "residual")
+        pp.title("Varying x and y")
+        pp.legend()
+        pp.show()
+
+
+    def visualize_residuals(self,res_func,guess_del):
+        x,y,theta = guess_del
+        xs = [x+.01*i for i in range(-200,200)]
+        vals = [np.array([[x_i], [y], [theta]]) for x_i in xs]
+        rs = [res_func(val) for val in vals]
+
+        pp.subplot(131)
+        pp.plot(xs,rs)
+        pp.title("Varying X")
+        pp.xlabel("x value")
+        pp.ylabel("residual")
+
+        ys = [y+.01*i for i in range(-200,200)]
+        vals = [np.array([[x], [y_i], [theta]]) for y_i in ys]
+        rs = [res_func(val) for val in vals]
+
+        pp.subplot(132)
+        pp.plot(xs,rs)
+        pp.title("Varying Y")
+        pp.xlabel("y value")
+        pp.ylabel("residual")
+
+        ts = [theta+.01*i for i in range(-200,200)]
+        vals = [np.array([[x], [y], [t_i]]) for t_i in ts]
+        rs = [res_func(val) for val in vals]
+
+        pp.subplot(133)
+        pp.plot(xs,rs)
+        pp.title("Varying Theta")
+        pp.xlabel("theta value")
+        pp.ylabel("residual")
+        pp.show()
+
+
+    def execute(self):
+        # run awwwwwwwwwwwwwwway
+        r = rospy.Rate(5)
+        while not(rospy.is_shutdown()):
+
+            old, new = self.lazer_sub.get_scans()
+            guess_del = self.odom_sub.get_deltas()
+
+            if old != None and new != None and guess_del != None:
+                angle = tf.transformations.euler_from_quaternion(self.current_quat)[2]
+                res_func = lambda vect: rsd.residual(old,new,vect[0,0],vect[1,0],vect[2,0],angle)
+                self.visualize_residuals_2(res_func,guess_del)
+
+                break
+            r.sleep()
+
+
+
+if __name__ == '__main__':
+    try:
+        lazerdom = Lazerdom()
+        lazerdom.execute()
+    except rospy.ROSInterruptException:
+        pass
